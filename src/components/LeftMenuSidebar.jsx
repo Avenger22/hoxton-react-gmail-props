d6538b6d1e6d9ae07fe48d8c82da41d683bce3ec
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2022-01-06 15:35:58 +0100

    divided to components but 1 bug

---
+++ src/components/LeftMenuSidebar.jsx
@@ -0,0 +1,41 @@
+function LeftMenuSidebar(props) {
+
+    return (
+        <nav className="left-menu">
+
+        <ul className="inbox-list">
+
+          <li
+            className={`item ${props.currentTab === 'inbox' ? 'active' : ''}`}
+            onClick={() => setCurrentTab('inbox')}
+          >
+            <span className="label">Inbox</span>
+            <span className="count">{props.unreadEmails.length}</span>
+          </li>
+
+          <li
+            className={`item ${props.currentTab === 'starred' ? 'active' : ''}`}
+            onClick={() => setCurrentTab('starred')}
+          >
+            <span className="label">Starred</span>
+            <span className="count">{props.starredEmails.length}</span>
+          </li>
+
+          <li className="item toggle">
+            <label htmlFor="hide-read">Hide read</label>
+            <input
+              id="hide-read"
+              type="checkbox"
+              checked={props.hideRead}
+              onChange={e => props.setHideRead(e.target.checked)}
+            />
+          </li>
+
+        </ul>
+
+      </nav>
+    )
+
+}
+
+export default LeftMenuSidebar
